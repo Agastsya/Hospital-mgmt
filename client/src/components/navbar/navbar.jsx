18ccc62ec48bcb155e21e32caa026ec4eb37495b
Agastsya Joshi <agastsya007@gmail.com>
2024-11-17 22:19:42 +0530

    Update navbar.jsx

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
             <a
-              href="/home"
+              href="/"
               className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
             >
               Home
